c7ce8ac30db94917415d1335ea619687eabf60db
EC2 Default User <ec2-user@ip-172-31-1-81.us-east-2.compute.internal>
2018-08-05 02:49:13 +0000

    first commit

---
+++ app/models/message.rb
@@ -0,0 +1,8 @@
+class Message < ApplicationRecord
+   
+   
+ validates :title , length: { maximum: 32 } , presence: true
+ validates :message , length: { maximum: 256 } , presence: true
+ validates :name , length: { maximum: 32 }
+ validates :weight , length: { maximum: 32 }
+end
